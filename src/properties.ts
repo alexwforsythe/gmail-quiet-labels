c91a76861a9ad9da2550254bc8d1561c110dbbee
Alex Forsythe <alexwforsythe@gmail.com>
2026-04-25 13:18:12 -0700

    Add persistent user settings and state management
    Introduces functions to save, load, and clear user-specific settings and
    state using a properties service. Enables configurable options and
    persistence for future sessions, supporting improved user experience and
    reliability.

---
+++ src/properties.ts
@@ -0,0 +1,60 @@
+import Log from './logger';
+
+export const settingsKey = 'USER_SETTINGS';
+export const stateKey = 'USER_STATE';
+export const defaultEvaluationIntervalHours = 12;
+
+export interface Settings {
+  // Filters
+  labelId: string;
+  excludeRead: boolean;
+  excludeImportant: boolean;
+
+  // Timer trigger
+  enableTimerTrigger: boolean;
+  intervalHours: number;
+}
+
+export interface State {
+  lastRunMs: number;
+}
+
+export interface Properties {
+  settings: Settings;
+  state: State;
+}
+
+export function saveSettings(settings: Settings) {
+  const val = JSON.stringify(settings);
+  PropertiesService.getUserProperties().setProperty(settingsKey, val);
+  Log.info('Saved settings', { settings });
+}
+
+export function saveState(state: State) {
+  const val = JSON.stringify(state);
+  PropertiesService.getUserProperties().setProperty(stateKey, val);
+  Log.info('Saved state', { state });
+}
+
+export function clearState() {
+  PropertiesService.getUserProperties().deleteProperty(stateKey);
+  Log.info('Cleared state');
+}
+
+export function loadProps(): Properties {
+  const { [settingsKey]: settings, [stateKey]: state } =
+    PropertiesService.getUserProperties().getProperties();
+  const props = {
+    settings: {
+      labelId: '',
+      excludeRead: false,
+      excludeImportant: false,
+      enableTimerTrigger: false,
+      intervalHours: defaultEvaluationIntervalHours,
+      ...(settings && JSON.parse(settings)),
+    },
+    state: state ? JSON.parse(state) : { lastRunMs: 0 },
+  };
+  Log.debug('Loaded props', { props });
+  return props;
+}
